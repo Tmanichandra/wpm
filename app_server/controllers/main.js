71d1ff66c9b8d6987bec82ab9c5304abf7a787e4
LandrumTrev <landrum.trevillian@gmail.com>
2019-05-12 00:06:12 -0400

    github and heroku reset

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -3,9 +3,9 @@
 
 // controller to render views/index.pug template with specific data,
 const index = (req, res) => {
-  // render views/index.pug, use "Bananafishbones" as the data for #{title}
+  // render views/index.pug, use "Tuatha Dé Danann" as the data for #{title}
   // route to app_server/views/.. for res.render is defined in app.js
-  res.render("index", { title: "Bananafishbones" });
+  res.render("index", { title: "Tuatha Dé Danann" });
 };
 
 module.exports = { index };
