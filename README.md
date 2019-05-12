a3b0e1a2eaf89400519588416929122945ae44d8
LandrumTrev <landrum.trevillian@gmail.com>
2019-05-12 19:08:20 -0400

    locations-list.pug basic content layout

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # mean-loc8r-1
+
+### Loc8r (part 1)
+
 MEAN stack exercise site (part 1) from Simon Holmes and Clive Harber's "Getting MEAN with Mongo, Express, Angular, and Node"
 
 [app: https://mean-loc8r-1.herokuapp.com/](https://mean-loc8r-1.herokuapp.com/)
--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,24 +1,22 @@
 // controller for /location routes, imported by routes/index.js 
+// route to view templates in app_server/views/.. for res.render is defined in app.js
 // ========================================================
 
 // controller for rendering the "/" home page index view (list of locations page)
 const homelist = (req, res) => {
-  // render views/index.pug, use "Home" as the data for #{title}
-  // route to view templates in app_server/views/.. for res.render is defined in app.js
-  res.render("index", { title: "Home" });
+  // render views/locations-list.pug, use "Home" as the data for #{title}
+  res.render("locations-list", { title: "Locations" });
 };
 
 // controller for rendering the "/location" page view
 const locationInfo = (req, res) => {
-  // render views/index.pug, use "Home" as the data for #{title}
-  // route to view templates in app_server/views/.. for res.render is defined in app.js
+  // render views/index.pug, use "Location info" as the data for #{title}
   res.render("index", { title: "Location info" });
 };
 
 // controller for rendering the "/location/review/new" page view
 const addReview = (req, res) => {
-  // render views/index.pug, use "Home" as the data for #{title}
-  // route to view templates in app_server/views/.. for res.render is defined in app.js
+  // render views/index.pug, use "Add review" as the data for #{title}
   res.render("index", { title: "Add review" });
 };
 
