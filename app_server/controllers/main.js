9b15effb589efd55fb0857130bd00cb951460bf9
LandrumTrev <landrum.trevillian@gmail.com>
2019-05-11 23:55:05 -0400

    silly name change

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -3,9 +3,9 @@
 
 // controller to render views/index.pug template with specific data,
 const index = (req, res) => {
-  // render views/index.pug, use "Monkeypants" as the data for #{title}
+  // render views/index.pug, use "Bananafishbones" as the data for #{title}
   // route to app_server/views/.. for res.render is defined in app.js
-  res.render("index", { title: "Monkeypants" });
+  res.render("index", { title: "Bananafishbones" });
 };
 
 module.exports = { index };
