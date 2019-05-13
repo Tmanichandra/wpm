d388b1eba99520aa7b789b274d72b62f7bbf317f
LandrumTrev <landrum.trevillian@gmail.com>
2019-05-12 22:34:36 -0400

    generic-text.pug setup for about view mockup

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,11 +1,11 @@
 // controller for other routes, imported by routes/index.js 
+// route to view templates in app_server/views/.. for res.render is defined in app.js
 // ========================================================
 
 // controller for rendering the "/about" page view
 const about = (req, res) => {
-  // render views/index.pug, use "About" as the data for #{title}
-  // route to view templates in app_server/views/.. for res.render is defined in app.js
-  res.render("index", { title: "About" });
+  // render views/generic-text.pug, use "About" as the data for #{title}
+  res.render("generic-text", { title: "About" });
 };
 
 module.exports = { 
