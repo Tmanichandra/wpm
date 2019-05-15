29e4090316c6c5680d45b7017969b6671d94f973
LandrumTrev <landrum.trevillian@gmail.com>
2019-05-15 01:26:22 -0400

    locations-list.pug moved data to controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,17 +1,41 @@
-// controller for /location routes, imported by routes/index.js 
+// controller for /location routes, imported by routes/index.js
 // route to view templates in app_server/views/.. for res.render is defined in app.js
 // ========================================================
 
 // controller for rendering the "/" home page index view (list of locations page)
 const homelist = (req, res) => {
   // 1st arg: name of the .pug file to render in /views
-  res.render("locations-list", { 
+  res.render("locations-list", {
     // 2nd arg: JS object containing data to render in the 1st arg page view
     title: "Loc8r - find a place to work with wifi",
     pageHeader: {
-      title: 'Loc8r',
-      strapline: 'Find places to work with wifi near you!'
-    } 
+      title: "Loc8r",
+      strapline: "Find places to work with wifi near you!"
+    },
+    sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake, or a pint? Let Loc8r help you find the place you're looking for, mate.",
+    locations: [
+      {
+        name: "Starcups",
+        address: "125 High Street, Reading, RG6 1PS",
+        rating: 3,
+        facilities: ["Hot drinks", "Food", "Premium wifi"],
+        distance: "100m"
+      },
+      {
+        name: "Cafe Hero",
+        address: "150 High Street, Reading, RG6 1PS",
+        rating: 4,
+        facilities: ["Hot drinks", "Premium wifi"],
+        distance: "200m"
+      },
+      {
+        name: "Burger Queen",
+        address: "175 High Street, Reading, RG6 1PS",
+        rating: 2,
+        facilities: ["Food", "Premium wifi"],
+        distance: "250m"
+      }
+    ]
   });
 };
 
@@ -27,7 +51,7 @@ const addReview = (req, res) => {
   res.render("location-review-form", { title: "Add review" });
 };
 
-module.exports = { 
+module.exports = {
   homelist,
   locationInfo,
   addReview
