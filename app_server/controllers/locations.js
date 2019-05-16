0fe90b2e26009700835ae2122f1f4e02f5393e09
LandrumTrev <landrum.trevillian@gmail.com>
2019-05-15 23:59:34 -0400

    location-info.pug remove data to controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -12,7 +12,8 @@ const homelist = (req, res) => {
       title: "Loc8r",
       strapline: "Find places to work with wifi near you!"
     },
-    sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake, or a pint? Let Loc8r help you find the place you're looking for, mate.",
+    sidebar:
+      "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake, or a pint? Let Loc8r help you find the place you're looking for, mate.",
     locations: [
       {
         name: "Starcups",
@@ -42,13 +43,64 @@ const homelist = (req, res) => {
 // controller for rendering the "/location" page view
 const locationInfo = (req, res) => {
   // render views/index.pug, use "Location info" as the data for #{title}
-  res.render("location-info", { title: "Location info" });
+  res.render("location-info", {
+    title: "Starcups",
+    pageHeader: {
+      title: "Loc8r"
+    },
+    sidebar: {
+      context: "is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.",
+      callToAction: "If you've been and you like it - or if you don't - please leave a review to help other people just like you."
+    },
+    location: {
+      name: "Starcups",
+      address: "125 High Street, Reading, RG6 1PS",
+      rating: 3,
+      facilities: ["Hot drinks", "Food", "Premium wifi"],
+      coords: { lat: 51.455041, lng: -0.9690884 },
+      openingTimes: [
+        {
+          days: "Monday - Friday",
+          opening: "7:00am",
+          closing: "7:00pm",
+          closed: false
+        },
+        {
+          days: "Saturday",
+          opening: "8:00am",
+          closing: "5:00pm",
+          closed: false
+        },
+        {
+          days: "Sunday",
+          closed: true
+        }
+      ],
+      reviews: [
+        {
+          author: "Simon Holmes",
+          rating: 5,
+          timestamp: "16 July 2013",
+          reviewText: "What a great place. I can't say enough good things about it."
+        },
+        {
+          author: "Charlie Chaplin",
+          rating: 3,
+          timestamp: "16 June 2013",
+          reviewText: "It was okay. Coffee wasn't great, but the wifi was fast."
+        }
+      ]
+    }
+  });
 };
 
 // controller for rendering the "/location/review/new" page view
 const addReview = (req, res) => {
   // render views/index.pug, use "Add review" as the data for #{title}
-  res.render("location-review-form", { title: "Add review" });
+  res.render("location-review-form", {
+    title: "Review Starcups on Loc8r",
+    pageHeader: { title: "Review Starcups" }
+  });
 };
 
 module.exports = {
